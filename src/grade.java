2ca5f4244888be051a3ed193c9414cddebb9063b
Faruh04 <gafurjanowf@gmail.com>
2026-06-25 01:06:23 +0300

    fixing

--- src/grade.java
+++ src/grade.java
@@ -1,2 +1,22 @@
+import java.util.ArrayList;
 public class grade {
+
+    private static int next_id = 123;
+
+
+    int id;
+    int student_id;
+    int section_id;
+    float value;
+
+        public grade(
+                int _student_id,
+                int _section_id,
+                float _value
+        ){
+            this.id = next_id++;
+            this.student_id = _student_id;
+            this.section_id = _section_id;
+            this.value = _value;
+        }
 }
--- src/section.java
+++ src/section.java
@@ -1,2 +1,39 @@
+import java.util.ArrayList;
+
 public class section {
+
+    private static int next_id = 123;
+
+
+        int id;
+        int subject_id;
+        String name;
+        String day;
+        String time;
+        String room;
+        ArrayList<Integer> student_ids;
+
+
+
+    public section(
+
+            int _subject_id,
+            String _name,
+            String _day,
+            String _time,
+            String _room,
+            ArrayList<Integer> _student_ids
+
+    ){
+
+            this.id = next_id++;
+            this.subject_id = _subject_id;
+            this.name = _name;
+            this.day = _day;
+            this.time = _time;
+            this.room = _room;
+            this.student_ids = _student_ids;
+
+    }
+
 }
