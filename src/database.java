64df3eaa4b51ef6916ee6d4c8033ef1f8348ca30
Faruh04 <gafurjanowf@gmail.com>
2026-06-26 00:22:05 +0300

    creating database and adding admin for future

--- src/database.java
+++ src/database.java
@@ -1,2 +1,45 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class database {
+
+
+
+public static HashMap<Integer, user> users = new HashMap<>();
+public static HashMap<Integer, subject> subjects = new HashMap<>();
+public static HashMap<Integer, section> sections = new HashMap<>();
+public static HashMap<Integer, grade> grades = new HashMap<>();
+
+
+
+
+
+static user admin = new user(
+
+        "admin",
+        "-----",
+        "admin",
+        "admin",
+        "-----",
+        "admin",
+        "-----"
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
